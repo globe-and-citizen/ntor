22937413224cc7cfba7ddd29440f08d4a51603d1
Veronika <58723287+interestIngc@users.noreply.github.com>
2025-05-03 10:04:03 +0200

    formatting

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,17 @@ fn generate_private_public_key_pair() -> PrivatePublicKeyPair {
     let pkcs8_doc = Ed25519KeyPair::generate_pkcs8(&random).unwrap();
     let key_pair = Ed25519KeyPair::from_pkcs8(pkcs8_doc.as_ref()).unwrap();
 
-    PrivatePublicKeyPair{
+    PrivatePublicKeyPair {
         private_key: pkcs8_doc.as_ref().to_vec(),
         public_key: key_pair.public_key().as_ref().to_vec(),
     }
 }
 
 fn main() {
-    let PrivatePublicKeyPair{ private_key, public_key} =
-        generate_private_public_key_pair();
+    let PrivatePublicKeyPair {
+        private_key,
+        public_key,
+    } = generate_private_public_key_pair();
 
     println!("Private key: {:?}", private_key);
     println!("Public key: {:?}", public_key);
--- src/test.rs
+++ src/test.rs
@@ -5,8 +5,14 @@ mod tests {
     #[test]
     fn test_generate_private_public_key_pair() {
         let key_pair = generate_private_public_key_pair();
-        assert!(!key_pair.private_key.is_empty(), "Private key should not be empty");
-        assert!(!key_pair.public_key.is_empty(), "Public key should not be empty");
+        assert!(
+            !key_pair.private_key.is_empty(),
+            "Private key should not be empty"
+        );
+        assert!(
+            !key_pair.public_key.is_empty(),
+            "Public key should not be empty"
+        );
 
         println!("Private key: {:?}", key_pair.private_key);
         println!("Public key: {:?}", key_pair.public_key);
